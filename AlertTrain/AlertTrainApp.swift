877ae6010464047bc297cb7d7bff0af1089752c0
danyaDerdan <110595066+danyaDerdan@users.noreply.github.com>
2024-11-01 19:48:19 +0300

    Initial Commit

---
+++ AlertTrain/AlertTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertTrainApp.swift
+//  AlertTrain
+//
+//  Created by Данил Толстиков on 01.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct AlertTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlertTrainTests/AlertTrainTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlertTrainTests.swift
+//  AlertTrainTests
+//
+//  Created by Данил Толстиков on 01.11.2024.
+//
+
+import Testing
+@testable import AlertTrain
+
+struct AlertTrainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
